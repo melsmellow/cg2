fee1431e6e6ea46527e7beef56ea80ee1a502ceb
Mel Carlo Iguis <melcarlo.iguis@gmail.com>
2021-12-28 02:29:26 +0800

    added a mockup data for staff Page

--- src/components/StaffComp.js
+++ src/components/StaffComp.js
@@ -1,6 +1,12 @@
 import React,{useContext} from 'react';
-import {Row , Col} from 'react-bootstrap';
+import {Row , Col, Table} from 'react-bootstrap';
 import AppContext from '../AppContext';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DoNotDisturbAltIcon from '@mui/icons-material/DoNotDisturbAlt';
+// Mock Data
+import {StaffData} from './StaffData'
+
+import staff2 from '../images/staff4.jpg';
 
 
 function StaffComp() {
@@ -18,6 +24,37 @@ function StaffComp() {
 							<Col md="10">
 								<h1>Staff</h1>
 							</Col>
+							<Col md="10" className="mx-auto">
+							<Table  hover className="table">
+						  <thead>
+						    <tr>
+						     
+							  <th>   </th>
+						      <th>Staff Name</th>
+						      <th>Position</th>
+						      <th>Status</th>
+							  <th>Availability</th>
+							  <th>Action</th>
+						    </tr>
+						  </thead>
+						  <tbody>
+						    {StaffData.map((val, key)=> {
+						    	return(
+						    		<tr key={key}>
+										<td><img id="profImg" src={staff2}></img></td>
+						    			<td>{val.firstName}{" "}{val.lastName}</td>
+						    			<td>{val.position}</td>
+						    			<td>{val.status}</td>
+										<td>{val.isAvailable ? <CheckCircleIcon/> : <DoNotDisturbAltIcon/>}</td>
+										<div id="staff-btn" className="btn">more details</div>
+						    			
+						    		</tr>
+						    		
+						    	)
+						    })}
+						  </tbody>
+						</Table>
+						</Col>
 						</div>
 					</Row>
 				</Col>
---
+++ src/components/StaffData.js
@@ -0,0 +1,47 @@
+import React from 'react';
+// Local Image
+
+
+export const StaffData = [
+	{
+		firstName: "John",
+		lastName: "Smith",
+        position: "Technician",
+		status: "on Call",
+        isAvailable : true,
+     
+	},
+	{
+		firstName: "Jane",
+		lastName: "Doe",
+        position: "Secretary",
+		status: "on Leave",
+        isAvailable : false
+    
+	},
+	{
+		firstName: "Juan",
+		lastName: "Dela Cruz",
+        position: "Front Desk",
+		status: "on Duty",
+        isAvailable : true
+     
+	},
+	{
+		firstName: "Pedro",
+		lastName: "Santos",
+        position: "Physician",
+		status: "on Duty",
+        isAvailable : true
+     
+	},
+	{
+		firstName: "John",
+		lastName: "Smith",
+        position: "Technician",
+		status: "on Leave",
+        isAvailable : false
+      
+	}
+
+]
